0e9c62bc5529b1e51e48a7cc6b7ec4f9e72a9967
matthewwoodruff <matthew.woodruff@me.com>
2015-11-10 21:47:50 +0000

    Zendesk play app update

--- zendesk/laptop/app.js
+++ zendesk/laptop/app.js
@@ -1,11 +1,63 @@
 (function() {
-
+  console.log(this.a);
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'setup',
+      'userGetRequest.done': 'showInfo',
+      'userGetRequest.fail': 'showError'
     },
 
-    doSomething: function() {
+    setup: function() {
+      var id = this.ticket().requester().id();
+      console.log(this.ajax('userGetRequest', id));
+    },
+
+    showInfo: function(data) {
+      this.formatDates(data);
+
+      if (data.user.organization_id != null) {
+        this.ajax('orgGetRequest', data.user.organization_id).then(
+          function(org_data) {
+            data.user.organization_name = org_data.organization.name;
+            this.switchTo('requester', data);
+          },
+          function() {
+            this.showError();
+          }
+        );
+      }
+
+      this.switchTo('requester', data);
+    },
+
+    showError: function() {
+      this.switchTo('error');
+    },
+
+    formatDates: function(data) {
+      var cdate = new Date(data.user.created_at);
+      var ldate = new Date(data.user.last_login_at);
+      data.user.created_at = cdate.toLocaleDateString();
+      data.user.last_login_at = ldate.toLocaleString();
+      return data;
+    },
+
+    requests: {
+      userGetRequest: function(id) {
+        return {
+          url: '/api/v2/users/' + id + '.json',
+          type:'GET',
+          dataType: 'json'
+        };
+      },
+
+      orgGetRequest: function(id) {
+        return {
+          url: '/api/v2/organizations/' + id + '.json',
+          type:'GET',
+          dataType: 'json'
+        };
+      }
     }
   };
 
