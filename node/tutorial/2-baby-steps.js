2a40a797b1b257bf98926be3ce13d56ca1dc232e
matthewwoodruff <matthew.woodruff@me.com>
2015-11-27 14:26:07 +0000

    Remove old node directory

--- node/tutorial/2-baby-steps.js
+++
@@ -1,5 +0,0 @@
-var total = process.argv.slice(2).reduce(function(previousValue, currentValue) {
-  return Number(previousValue) + Number(currentValue);
-});
-
-console.log(total);
--- node/tutorial/3-my-first-io.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-console.log(process.argv[1])
-
-var buffer = fs.readFileSync(process.argv[1]);
-
-console.log(buffer.toString().split("\n").length)
