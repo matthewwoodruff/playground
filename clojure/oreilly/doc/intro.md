4e32f41bed6f3ba4f9040aa20394b36fb6d86f8b
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-12-01 10:41:47 +0100

    Clojure app in intellij

---
+++ clojure/oreilly/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to oreilly
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/oreilly/test/oreilly/core_test.clj
@@ -0,0 +1,7 @@
+(ns oreilly.core-test
+  (:require [clojure.test :refer :all]
+            [oreilly.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
