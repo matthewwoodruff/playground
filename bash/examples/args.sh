4c699ce57c173d49ac5723947999427d164591e6
Matt Woodruff <mwoodruf@thoughtworks.com>
2017-01-29 11:43:14 +0100

    Update to args bash script

--- bash/examples/args.sh
+++ bash/examples/args.sh
@@ -6,11 +6,21 @@ echo "\$0 gives $0 - this is the first arg"
 echo "\$* gives $* - this is a string of all args"
 echo "\${@:2} gives ${@:2} - all args from 2 onwards as array"
 
+echo Printing array
 for i in $@
 do
-    echo $i
+    echo "$i"
 done
+echo
 
+echo Printing array string
+for a in "$@"
+do
+    echo "$a"
+done
+echo
+
+echo Printing select arguments
 for j in "${@:2}"
 do
     echo $j
