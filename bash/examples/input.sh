4cb9038cfff9f618786062707f3ba8e842f7b4ab
Matt Woodruff <mwoodruf@thoughtworks.com>
2017-01-29 09:30:57 +0100

    bash input example with array input

--- bash/examples/input.sh
+++ bash/examples/input.sh
@@ -5,3 +5,13 @@ echo "Type your name"
 read NAME
 
 echo "Your name is" $NAME
+
+echo Type some words
+read -a ARRAY
+
+echo "All words will be in an array using the -a flag " $ARRAY
+
+for word in $ARRAY
+do
+    echo $word
+done
