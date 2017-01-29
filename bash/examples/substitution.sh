49bc19ee758c235b7794be40db7711eb1b642934
Matt Woodruff <mwoodruf@thoughtworks.com>
2017-01-29 17:45:52 +0100

    Update to bash substitution example

--- bash/examples/substitution.sh
+++ bash/examples/substitution.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-echo ${DATE:-dateNotSet}
-echo ${DATE:?date not set, setting to tomorrow}
-${DATE:=tomorrow}
+echo ${DATE:-no date set so echo this message without setting the date variable}
+echo ${DATE:?date not set, please give a value}
+# ${DATE:=tomorrow} Sets the DATE var to tomorrow if not set
+echo ${@:1:1} # prints arguments with offset for length 1 (in this case the first argument to the script)
 echo $DATE
